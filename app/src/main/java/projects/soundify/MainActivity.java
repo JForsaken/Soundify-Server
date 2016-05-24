7f113a67606c5e4fe4c362a275aaffa9a63dd349
joseph_salem <joseph.salem.1@etsmtl.net>
2016-05-24 18:33:24 -0400

    added the buttons

--- app/src/main/java/projects/soundify/MainActivity.java
+++ app/src/main/java/projects/soundify/MainActivity.java
@@ -2,6 +2,7 @@ package projects.soundify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void play() {
+
+    }
+
+    public void pause() {
+
+    }
+
+    public void next() {
+
+    }
+
+    public void previous() {
+
     }
 }
