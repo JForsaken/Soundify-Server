07902a268b2f0a4f525792eda03b95bbd4461d9f
Justin D'Errico <justin.derrico1991@gmail.com>
2016-05-25 20:53:15 -0400

    Added the basic song manipulations

--- app/src/main/java/projects/soundify/MainActivity.java
+++ app/src/main/java/projects/soundify/MainActivity.java
@@ -1,30 +1,29 @@
 package projects.soundify;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    HttpController _httpController = HttpController.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _httpController.init();
     }
 
-    public void play() {
-
+    public void play(View view) {
+        _httpController.play();
     }
 
-    public void pause() {
-
+    public void pause(View view) {
+        _httpController.stop();
     }
 
-    public void next() {
+    public void next(View view) { _httpController.next(); }
 
-    }
-
-    public void previous() {
-
-    }
+    public void previous(View view) { _httpController.previous(); }
 }
