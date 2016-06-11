8c5657818c2953b2459ea29ef5052b185ecd378c
joseph_salem <joseph.salem.1@etsmtl.net>
2016-06-11 19:37:25 -0400

    Ajoute de l'affichage du IP du serveur.

--- app/src/main/java/projects/soundifyserver/MainActivity.java
+++ app/src/main/java/projects/soundifyserver/MainActivity.java
@@ -1,11 +1,21 @@
 package projects.soundifyserver;
 
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.Formatter;
+import android.widget.TextView;
+
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
 
     private Server _server;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +26,25 @@ public class MainActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
         _server = new Server();
+
+        WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+        byte[] ipAddress = BigInteger.valueOf(wm.getConnectionInfo().getIpAddress()).toByteArray();
+
+        try {
+            InetAddress myaddr = InetAddress.getByAddress(ipAddress);
+            StringTokenizer hostaddr = new StringTokenizer(myaddr.getHostAddress(), ".");
+            String ipaddr = "";
+
+            while (hostaddr.hasMoreTokens()) {
+                ipaddr = hostaddr.nextToken()+ "." + ipaddr;
+            }
+
+            TextView txtIP = (TextView) findViewById(R.id.txtIP);
+            txtIP.setText(ipaddr);
+        }
+        catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
